8a436fd0c5e69c859b4f23f3779dacb7a22f5984
rkmjld2 <rkmjld2@gmail.com>
2026-09-07 14:31:24 +0530

    Update connection.php

--- crud3/connection.php
+++ crud3/connection.php
@@ -1,14 +1,30 @@
+
 <?php
 
-$con = mysqli_connect(
+$con = mysqli_init();
+
+mysqli_ssl_set(
+    $con,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL
+);
+
+mysqli_real_connect(
+    $con,
     getenv("DB_HOST"),
     getenv("DB_USER"),
     getenv("DB_PASSWORD"),
-    getenv("DB_NAME"),
-    (int)getenv("DB_PORT")
+    getenv("CRUD3_DB_NAME"),
+    (int)getenv("DB_PORT"),
+    NULL,
+    MYSQLI_CLIENT_SSL
 );
 
 if (mysqli_connect_errno())
 {
     echo 'Database Connection Error';
 }
+
