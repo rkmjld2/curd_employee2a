dad8a05c4d45914c4349c03411e53f68da1c260c
rkmjld2 <rkmjld2@gmail.com>
2026-09-07 14:51:35 +0530

    Update login.php

--- login.php
+++ login.php
@@ -50,7 +50,7 @@ $allowed_applications = [
     "index.php",
     "payroll.php",
     "controller.php",
-   "crud3/index.php",
+   "/crud3/index.php",
   "https://esp-switch5e-remote.onrender.com"
 ];
 
